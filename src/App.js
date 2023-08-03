53b1a10cfcce38c68cda38f6495973fefc63bc19
Mitchell Kabenda <mstacy.kabenda@gmail.com>
2023-08-03 01:30:59 -0500

    Html/CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="calculator">
+      <div className="display">
+        <span>0</span>
+      <div className="operators">
+      <button>+</button>
+      <button>-</button>
+      <button>X</button>
+      <button>/</button>
+      </div>
+
+      <div className="digits">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+        <button>0</button>
+        <button>.</button>
+        <button className="equal">=</button>
+      </div>
+      </div>
+     </div>
     </div>
   );
 }
